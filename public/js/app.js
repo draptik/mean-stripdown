78b58f5b71f6e7963f5b0410e8b1fcd79a3ee092
Patrick Drechsler <socialcoding@pdrechsler.de>
2014-01-03 17:45:37 +0100

    added players and players-filter

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,11 @@
-window.app = angular.module('ngFantasyFootball', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'ngff.controllers', 'ngff.directives', 'ngff.services']);
+window.app = angular.module('ngFantasyFootball', [
+	'ngCookies', 
+	'ngResource', 
+	'ui.bootstrap', 
+	'ngRoute', 
+	'ngff.controllers', 
+	'ngff.directives', 
+	'ngff.services']);
 
 // bundling dependencies
 window.angular.module('ngff.controllers', [
@@ -6,10 +13,12 @@ window.angular.module('ngff.controllers', [
 	'ngff.controllers.index',
 	'ngff.controllers.nfl',
 	'ngff.controllers.leagues',
-	'ngff.controllers.fantasyTeams']);
+	'ngff.controllers.fantasyTeams',
+	'ngff.controllers.players']);
 
 window.angular.module('ngff.services', [
 	'ngff.services.global',
 	'ngff.services.nfl',
 	'ngff.services.leagues',
-	'ngff.services.fantasyTeams']);
+	'ngff.services.fantasyTeams',
+	'ngff.services.players']);
